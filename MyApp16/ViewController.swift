2d981aeb2b69266545c6a166178532f1e064e16d
Brad <brad@brad.tw>
2017-09-26 12:05:26 +0800

    2017092601

--- MyApp16/ViewController.swift
+++ MyApp16/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, URLSessionDownloadDelegate {
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBAction func doTest1(_ sender: Any) {
+        let url = URL(string: "http://www.planwallpaper.com/static/images/abstract-background-design.jpg")
+        
+        let config = URLSessionConfiguration.background(withIdentifier: "myid")
+        let session = URLSession(configuration: config, delegate: self, delegateQueue: nil)
+        
+        let task = session.downloadTask(with: url!)
+        task.resume()
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        print("finish download")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
