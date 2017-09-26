207508b5ad7b7765a5bbbd0e52c13b027ceafbdc
Brad <brad@brad.tw>
2017-09-26 13:28:03 +0800

    2017092605

--- MyApp16/ViewController.swift
+++ MyApp16/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController, URLSessionDownloadDelegate {
     @IBOutlet weak var imgView: UIImageView!
+    private var q1 = DispatchQueue(label: "q1", qos: DispatchQoS.background, attributes: DispatchQueue.Attributes.concurrent)
+    
+    @IBAction func doTest2(_ sender: Any) {
+        let url = URL(string: "http://www.planwallpaper.com/static/images/abstract-background-design.jpg")
+        q1.async {
+            if let data = try? Data(contentsOf: url!) {
+                let img = UIImage(data: data)
+                DispatchQueue.main.async {
+                    self.imgView.image = img
+                }
+            }
+        }
+    }
     
     @IBAction func doTest1(_ sender: Any) {
         let url = URL(string: "http://www.planwallpaper.com/static/images/abstract-background-design.jpg")
@@ -23,6 +36,15 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
     
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
         print("finish download")
+        
+        if let data = try? Data(contentsOf: location) {
+            let img = UIImage(data: data)
+            DispatchQueue.main.async {
+                self.imgView.image = img
+            }
+        }
+        
+        
     }
 
     override func viewDidLoad() {
